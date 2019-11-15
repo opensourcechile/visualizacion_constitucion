5975b6944379359d2157907e064063a07e9260c5
Eduardo Ramírez <ejramire@uc.cl>
2019-11-16 00:41:31 +0100

    Deploy updates

--- gh_pages_deploy.sh
+++ gh_pages_deploy.sh
@@ -9,7 +9,7 @@ echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
 
 echo -e "\033[0;32mChecking out $branch....\033[0m"
-git worktree add $directory $branch
+git worktree add -f $directory $branch
 
 echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
